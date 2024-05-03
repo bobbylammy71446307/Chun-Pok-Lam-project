55dbfb3ea17ba5b9997dbe19c2a8bd89ea54b6fa
Bob Lam <104420320+bobbylammy71446307@users.noreply.github.com>
2024-05-03 12:48:41 +0800

    Add files via upload

--- fundementals of ai/algorithm search project/search_function.py
+++ fundementals of ai/algorithm search project/search_function.py
@@ -17,7 +17,11 @@ class Node():
             self.depth=parent.depth+1
     
     def child_node(self,new_map, distance,direction):
-        next_node = Node(map=new_map, parent=self, path_cost=self.depth+1+distance,direction=direction)
+        dir_cost=0
+        if self.parent is not None and self.parent.direction != direction:
+           dir_cost=10
+
+        next_node = Node(map=new_map, parent=self, path_cost=self.depth+1+distance+dir_cost,direction=direction)
         return next_node
 
     def path(self):
@@ -45,11 +49,30 @@ class Problem_solving():
                       [coordinate[0], coordinate[1]-1], 
                       [coordinate[0], coordinate[1]+1]]
         for i in range (0,len(direction),2):
-            if self.node.map[coordinate[0]+direction[i][0]][coordinate[1]+direction[i][1]] == 0 and self.node.map[coordinate[0]+direction[i+1][0]][coordinate[1]+direction[i+1][1]] == 0:
+            element_1=self.node.map[coordinate[0]+direction[i][0]][coordinate[1]+direction[i][1]]
+            element_2=self.node.map[coordinate[0]+direction[i+1][0]][coordinate[1]+direction[i+1][1]]
+            accept=[[0,0],[0,4],[4,0]]
+            if [element_1,element_2] in accept:
                 valid_candidates += [candidates[i]]
                 valid_candidates += [candidates[i+1]]
                 valid_direction += [direction[i]]
                 valid_direction += [direction[i+1]]
+        return valid_candidates, valid_direction
+
+
+
+    def mouse_action(self,coordinate):
+        valid_candidates = []
+        valid_direction = []
+        candidates = [[coordinate[0]-1, coordinate[1]], 
+                      [coordinate[0]+1, coordinate[1]],
+                      [coordinate[0], coordinate[1]-1], 
+                      [coordinate[0], coordinate[1]+1]]
+        for i in range (0,len(direction)):
+            element_1=self.node.map[coordinate[0]+direction[i][0]][coordinate[1]+direction[i][1]]
+            if element_1==0:
+                valid_candidates += [candidates[i]]
+                valid_direction += [direction[i]]
         return valid_candidates, valid_direction
 
     def move(self,coordinate,action):
@@ -64,20 +87,28 @@ class Problem_solving():
         child=self.node.child_node(new_map,distance,direction)
         return child
 
-    def get_egg(self):
+    def get_element(self,is_egg):
+        if is_egg:
+            element=3
+        else:
+            element=2
         for i in range (len(self.node.map)):
             for j in range(len(self.node.map[i])):
-                if self.node.map[i][j]==3:
+                if self.node.map[i][j]==element:
                     break
-            if self.node.map[i][j]==3:
+            if self.node.map[i][j]==element:
                     break
         return [i,j]
     
-    def get_solution(self,destination_coordinate,q_action):
-        egg_coordinate=self.get_egg()
-        validcandidates, validdirection = self.action(egg_coordinate)
+    def get_solution(self,destination_coordinate,q_action,is_egg=True):
+        coordinate=self.get_element(is_egg)
+        if is_egg:
+            validcandidates, validdirection = self.action(coordinate)
+        else:
+            validcandidates,validdirection= self.mouse_action(coordinate)
         for i in range (len(validcandidates)):
-            q_action.push(self.expand(egg_coordinate,validcandidates[i],destination_coordinate,validdirection[i]))
+            next_node=self.expand(coordinate,validcandidates[i],destination_coordinate,validdirection[i])
+            q_action.push(next_node)
 
 
 
@@ -117,7 +148,7 @@ class Map():
                 block.append(wall)      
         for i in range (1,len(block)):
             map[block[i][0]][block[i][1]]=1
-        return map
+        return mouse_coordinate,map
 
 
     def fill_coordinate(self,map,list):
@@ -151,6 +182,12 @@ class Map():
         map[egg_coordinate[0]][egg_coordinate[1]]=0
         map[hole_coordinate[0]][hole_coordinate[1]]=3
         return map
+    
+    def mouse_complete(self,map,mouse,start):
+        map[mouse[0]][mouse[1]]=0
+        map[start[0]][start[1]]=2
+        return map
+    
     def make_partial(self, map, egg_coordinate,hole_coordinate):
         new_map=deepcopy(map)
         for i in range(len(map)):
@@ -206,12 +243,12 @@ class Map():
         return egg_list
     
     def map_init(self):
-        mapping=self.map_building()
+        mouse,mapping=self.map_building()
         coordinate_list=self.get_coordinate(mapping)
         self.fill_coordinate(mapping,coordinate_list)
         print(mapping)
         map_valid=m.map_validation(mapping,coordinate_list)
-        return mapping,coordinate_list,map_valid
+        return mapping,coordinate_list,map_valid,mouse
 
 
         
@@ -220,25 +257,50 @@ if __name__ == "__main__":
     map_valid=False
     m=Map()
     while map_valid==False:
-        mapping,coordinate_list,map_valid=m.map_init()  
+        mapping,coordinate_list,map_valid,mouse=m.map_init()  
         if map_valid==False:
             continue
         egg_list=m.get_egg_prio()
         hole_list=coordinate_list[4:]
+        solve_map=deepcopy(mapping)
         for i in range (len(egg_list)):
             egg=egg_list[i]
             hole=m.get_close_hole(egg,hole_list)
-            partial_map=m.make_partial(mapping,egg,hole)
+            partial_map=m.make_partial(solve_map,egg,hole)
             completed_map=m.make_completed(deepcopy(partial_map),egg,hole)
-            q_action=PriorityQueue(Node(mapping))
+            q_action=PriorityQueue(Node(partial_map))
             while q_action.empty() is False:
                 solve=Problem_solving(q_action.pop())
                 solve.get_solution(hole,q_action)
                 if (solve.node.map==completed_map).all():
+                    print("solved")
                     break
-            mapping[egg[0]][egg[1]]=0
-            mapping[hole[0]][hole[1]]=1
-            print("egg 1 solved")
-            print(solve.node.path())            
+            if q_action.empty():
+                map_valid==False
+                continue
+            path=solve.node.path()
 
+            egg_start=[egg[0]-path[0][0],egg[1]-path[0][1]]
+            complete_start_map=m.mouse_complete(deepcopy(partial_map),mouse,egg_start)
+            q_egg_action=PriorityQueue(Node(partial_map))
+            while q_egg_action.empty() is False:
+                start=Problem_solving(q_egg_action.pop())
+                start.get_solution(egg_start,q_egg_action,is_egg=False)
+                if (start.node.map==complete_start_map).all():
+                    print("started")
+                    break;
+            if q_egg_action.empty():
+                map_valid==False
+                continue
+            start_path=start.node.path()
+            start_path+=path
+            print(start_path)
+
+
+            solve_map[egg[0]][egg[1]]=0
+            solve_map[hole[0]][hole[1]]=1
+            mouse=[hole[0]-path[-1][0],hole[1]-path[-1][1]]
+            solve_map[mouse[0]][mouse[1]]==2
+            print(f"egg {i+1} {egg} solved")
+            print(start_path)            
 
